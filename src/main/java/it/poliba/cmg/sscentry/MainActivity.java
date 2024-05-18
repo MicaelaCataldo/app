654e274aad8fd1a0f8899118e3b7ed4051aa4f0b
MicaelaCataldo <miki.cataldo14@gmail.com>
2024-05-18 12:01:02 +0200

    aggiunte librerie, istanza primo statement

--- src/main/java/it/poliba/cmg/sscentry/MainActivity.java
+++ src/main/java/it/poliba/cmg/sscentry/MainActivity.java
@@ -10,22 +10,41 @@ import android.os.Bundle;
 
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Literal;
+import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.Statement;
-import org.eclipse.rdf4j.model.vocabulary.RDF4J.*;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.DynamicModelFactory;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+
+
 
 public class MainActivity extends AppCompatActivity {
-    String disco = "http://rdf-vocabulary.ddialliance.org/discovery";
-    IRI bob = iri("http://example.org/bob");
-    IRI nameProp = iri("http://example.org/name");
-    Literal bobsName = literal("Bob");
-    Literal bobsAge = literal(42);
+    // URL del vocabolario RDF
+    public static final String DISCO = "http://rdf-vocabulary.ddialliance.org/discovery#";
+    // Crea un ValueFactory per creare oggetti RDF
+    ValueFactory factory = SimpleValueFactory.getInstance();
 
-    Statement st = statement(bob, nameProp, bobsName, null);
+    IRI QuestionnaireUCLASCTC = iri(factory, DISCO+"Questionnaire");
+    IRI Question1 = iri(factory, DISCO+"Question");
+    IRI question = iri(factory, DISCO+"question");
+    Statement statement = statement(QuestionnaireUCLASCTC, question,Question1,null);
+
+    DynamicModelFactory dynamicModelFactory = new DynamicModelFactory();
+    Model model = dynamicModelFactory.createEmptyModel();
+
+    // add an RDF statement
+    model.add(statement);
+    // add another RDF statement by simply providing subject, predicate, and object.
+    model.add(QuestionnaireUCLASCTC, question, Question1);
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
     }
 }
