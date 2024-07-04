18ac4c4e0291aa4ec336d124dd3a0cc5aa8b4b3a
MicaelaCataldo <miki.cataldo14@gmail.com>
2024-07-04 10:55:29 +0200

    eliminazione textview main

--- src/main/java/it/poliba/cmg/sscentry/MainActivity.java
+++ src/main/java/it/poliba/cmg/sscentry/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = findViewById(R.id.textView);
         Button button1 = findViewById(R.id.button1);
         button1.setText("Submit UCLA Questionnaire");
         Button button2 = findViewById(R.id.button2);
